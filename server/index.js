d7bb44c65e9f6f578b51906f864f8c1dbebf1ab9
Lucas Paiva <lucasp4iva@gmail.com>
2023-03-17 21:38:58 -0300

    feat: add routes

--- server/index.js
+++ server/index.js
@@ -11,4 +11,13 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}))
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cors());
+
+/* Routes */
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/saler", salesRoutes);
