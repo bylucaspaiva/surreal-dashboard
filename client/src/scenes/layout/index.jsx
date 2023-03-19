1115a41438ed04bbe472123715f8cdfbbbbd1d4d
Lucas Paiva <lucasp4iva@gmail.com>
2023-03-19 15:44:16 -0300

    feat: add responsiviness to sidebar and navbar

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -3,13 +3,26 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Navbar from "components/Navbar"
+import Sidebar from "components/Sidebar"
 
 
 const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+
   return (
-    <Box width={`100%`} height={`100%`}>
+    <Box display={isNonMobile ? 'flex' : 'block'} width={`100%`} height={`100%`}>
+      <Sidebar 
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSideBarOpen={isSideBarOpen}
+        setIsSideBarOpen={setIsSideBarOpen}
+      />
       <Box>
-        <Navbar />
+        <Navbar 
+          isSideBarOpen={isSideBarOpen}
+          setIsSideBarOpen={setIsSideBarOpen}
+        />
         <Outlet />
       </Box>
     </Box>
