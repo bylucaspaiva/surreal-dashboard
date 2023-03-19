64d9d5c65da9c8229a88c33306c85c60f55621bf
Lucas Paiva <lucasp4iva@gmail.com>
2023-03-19 01:29:13 -0300

    feat: add theme provider with handle modes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-import { CssBaseline,  } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+import { useMemo } from 'react';
+
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
   return (
     <div className="App">
-      
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
   );
 }
