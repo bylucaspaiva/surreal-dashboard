8a7cdb5c522302fd0849b28d527eebb1c2a36036
Lucas Paiva <lucasp4iva@gmail.com>
2023-03-19 15:26:06 -0300

    feat: add left side of navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,13 +6,47 @@ import {
   Search, 
   SettingsOutlined, 
   ArrowDropDownOutlined 
-} from '@mui/icons-material'
+} from '@mui/icons-material';
+import FlexBetween from './FlexBetween';
+import { useDispatch } from 'react-redux';
+import { setMode } from 'state';
+import profileImage from 'assets/profile.jpg';
+import { AppBar, IconButton, InputBase, Toolbar, useTheme } from '@mui/material';
+
 
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const theme = useTheme()
   return (
-    <div>Navbar</div>
-  )
-}
+    <AppBar 
+    sx={{
+      position: "static",
+      background: "none",
+      boxShadow: "none"
+    }}
+    >
+      <Toolbar sx={{ justifyContent: "space-between"}}>
+        {/* Left side */}
+        <FlexBetween>
+          <IconButton onClick={() => console.log("open/close side bar")} >
+            <MenuIcon />
+          </IconButton>
+          <FlexBetween 
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1rem 1.5rem"
+          >
+            <InputBase placeholder='Search...' />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Navbar
