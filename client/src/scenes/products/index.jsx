b3f3c302c69e50c923d9dfaadac14b5e4a8be076
Lucas Paiva <lucasp4iva@gmail.com>
2023-03-26 21:50:34 -0300

    add product ath to app

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import { Box, Card, CardActions, CardContent, Collapse, Button, Tipography, Rating, useTheme, useMediaQuery } from "@mui/material";
 import { useGetProductsQuery } from 'state/api';
+import Header from 'components/Header';
 
 
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <Box>
+      <Header title="PRODUCTS" subtitle="See your list of products." />
+    </Box>
   )
 }
 
