3963da97575aff727f7cc97916abfe712c710cb8
Lucas Paiva <lucasp4iva@gmail.com>
2023-03-26 21:41:36 -0300

    feat: add products component

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { useMemo } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "scenes/dashboard";
 import Layout from 'scenes/layout';
+import Products from "scenes/products";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
---
+++ client/src/scenes/products/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Box, Card, CardActions, CardContent, Collapse, Button, Tipography, Rating, useTheme, useMediaQuery } from "@mui/material";
+
+const Products = () => {
+  return (
+    <div>Products</div>
+  )
+}
+
+export default Products
