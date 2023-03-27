fb5350120c8828b38f4acc74639de7381feadb5e
Lucas Paiva <lucasp4iva@gmail.com>
2023-03-27 17:12:21 -0300

    feat: create getTransactions endpoint

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -16,10 +16,18 @@ export const api = createApi({
     getCustomers: build.query({
       query: () => "client/customers",
       providesTags: ["Customers"],
+    }),
+    getTransactions: build.query({
+      query: ({page, pageSize, sort, search}) => ({
+        url: "client/transactions",
+        method: "GET",
+        params: {page, pageSize, sort, search},
+      }),
+      providesTags: ["Transactions"]
     })
   }),
 });
 
 export const {
-  useGetUserQuery, useGetProductsQuery, useGetCustomersQuery
+  useGetUserQuery, useGetProductsQuery, useGetCustomersQuery, useGetTransactionsQuery
 } = api;
