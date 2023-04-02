e9d750cc98ca43b1a4efa55824b1432115f4b10f
Lucas Paiva <lucasp4iva@gmail.com>
2023-04-02 18:12:55 -0300

    feat: add controller to get dashboars stats

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,4 +1,7 @@
 import User from "../models/User.js";
+import OverallStat from "../models/OverallStat.js";
+import Transaction from "../models/Transaction.js";
+
 
 export const getUser = async (req, res) => {
   try {
@@ -9,3 +12,47 @@ export const getUser = async (req, res) => {
     res.status(404).json({message: error.message})
   }
 }
+
+export const getDashboardStats = async (req, res) => {
+  try{
+    //hardcoded values
+    const currentMonth = "August";
+    const currentYear = 2021;
+    const currentDay = '2021-08-15';
+
+    //recent transactions 
+    const transactions = await Transaction.find().limit(50).sort({ createdOn: -1});
+
+    //overall stats
+    const overallStat = await OverallStat.find({year: currentYear});
+
+    const {
+      totalCustomers,
+      yearlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      salesByCategory
+    } = overallStat[0]
+
+    const thisMonthStat = overallStat[0].monthlyData.find(({month}) => {
+      return month === currentMonth;
+    })
+
+    const todayStat = overallStat[0].dailyData.find(({date}) => {
+      return date === currentDay;
+    })
+
+    res.status(200).json({
+      totalCustomers,
+      yearlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      salesByCategory,
+      thisMonthStat,
+      todayStat,
+      transactions
+    })
+  }catch (error) {
+    res.status(404).json({message: error.message})
+  }
+} 
