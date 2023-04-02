41f19482ddbf33560500d4da5582b00f447385ec
Lucas Paiva <lucasp4iva@gmail.com>
2023-04-02 18:27:25 -0300

    feat: add header and mui component to dasjboard

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,8 +1,92 @@
-import React from 'react'
+import React from 'react';
+import FlexBetween from 'components/FlexBetween';
+import Header from 'components/Header';
+import { 
+  DownloadOutlined, 
+  Email, 
+  PointOfSale, 
+  PersonAdd, 
+  Traffic
+ } from '@mui/icons-material';
+
+ import {
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  useMediaQuery
+ } from "@mui/material";
+
+ import { DataGrid } from '@mui/x-data-grid';
+ import BreakdownChart from 'components/BreakdownChart';
+ import OverviewChart from 'components/OverviewChart';
+ import { useGetDashboardQuery } from 'state/api';
+
 
 const Dashboard = () => {
+
+  const theme= useTheme();
+  const isNonMediumScreens = useMediaQuery("(min-width: 1200px)")
+  const { data, isLoading} = useGetDashboardQuery();
+  console.log("data1", data);
+
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "userId",
+      headerName: "User ID",
+      flex: 1,
+    },
+    {
+      field: "createdAt",
+      headerName: "CreatedAt",
+      flex: 1,
+    },
+    {
+      field: "products",
+      headerName: "# of Products",
+      flex: 0.5,
+      sortable: false,
+      renderCell: (params) => params.value.length
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => `${Number(params.value).toFixed(2)}`
+    },
+    {
+      field: "role",
+      headerName: "Role",
+      flex: 0.5,
+    },
+  ]
+
   return (
-    <div>Dashboard</div>
+    <Box m="1.5rem 2.5rem">
+      <FlexBetween>
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: theme.palette.secondary.light,
+              color: theme.palette.background.alt,
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px"
+            }}
+          >
+            <DownloadOutlined sx={{mr: "10px"}}/>
+            Download Reports
+          </Button>
+        </Box>
+      </FlexBetween>
+    </Box>
   )
 }
 
